3aeec78690873e789830afa8b2c707a3f0473c84
zhaofei <micoefee@gmail.com>
2022-03-20 01:33:18 +0800

    几何变换：模拟翻页效果

--- app/src/main/java/com/micoe/xfermode/CameraView.kt
+++ app/src/main/java/com/micoe/xfermode/CameraView.kt
@@ -14,14 +14,17 @@ class CameraView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private val bitmap = getAvatar(BITMAP_SIZE)
     private val camera = Camera()
     private val clipped = Path().apply {
-        addOval(BITMAP_PADDING,
+        addOval(
+            BITMAP_PADDING,
             BITMAP_PADDING,
             BITMAP_PADDING + BITMAP_SIZE,
-            BITMAP_PADDING + BITMAP_SIZE ,Path.Direction.CCW)
+            BITMAP_PADDING + BITMAP_SIZE, Path.Direction.CCW
+        )
     }
 
     init {
-        camera.rotateX(30f)
+        camera.rotateX(40f)
+        camera.setLocation(0f, 0f, -5 * resources.displayMetrics.density)
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -34,12 +37,23 @@ class CameraView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 //        )
 //        canvas.clipPath(clipped)
 
+        //上半部分
+        canvas.save()
+        canvas.translate(BITMAP_PADDING + BITMAP_SIZE / 2, BITMAP_PADDING + BITMAP_SIZE / 2)
+        canvas.clipRect(-BITMAP_SIZE / 2, -BITMAP_SIZE / 2, BITMAP_SIZE / 2, 0f)
+        canvas.translate(-(BITMAP_PADDING + BITMAP_SIZE / 2), -(BITMAP_PADDING + BITMAP_SIZE / 2))
+        canvas.drawBitmap(bitmap, BITMAP_PADDING, BITMAP_PADDING, paint)
+        canvas.restore()
+
         //几何变换
+        //下半部分
+        canvas.save()
         canvas.translate(BITMAP_PADDING + BITMAP_SIZE / 2, BITMAP_PADDING + BITMAP_SIZE / 2)
         camera.applyToCanvas(canvas)
+        canvas.clipRect(-BITMAP_SIZE / 2, 0f, BITMAP_SIZE / 2, BITMAP_SIZE / 2)
         canvas.translate(-(BITMAP_PADDING + BITMAP_SIZE / 2), -(BITMAP_PADDING + BITMAP_SIZE / 2))
-
         canvas.drawBitmap(bitmap, BITMAP_PADDING, BITMAP_PADDING, paint)
+        canvas.restore()
     }
 
     private fun getAvatar(width: Float): Bitmap {
